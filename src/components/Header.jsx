5871ff67b407063a9113f34fa4f2e6b860cbd9f6
76248usa <elsabecrous@Elsabes-MBP.lan>
2023-07-26 11:06:16 -0500

    create sign-in,sign-up,forgot password pages and OAuth component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,9 +11,9 @@ const pathMatchRoute = (route) => {
         }
     }
   return (
-    <div>
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
         <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
-            <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+            <div >
                 <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" 
                 alt="logo" className="h-5 cursor-pointer" 
                 onClick = {() => navigate('/')}/>
@@ -29,6 +29,7 @@ const pathMatchRoute = (route) => {
                     text-gray-400 border-b-4 border-b-transparent
                     ${pathMatchRoute('/offers') && '!text-black !border-b-red-500'}`}
                     onClick={() => navigate('/offers')}>Offers</li>
+                    
                     <li className={`py-3 text-sm font-semibold
                     text-gray-400 border-b-4 border-b-transparent
                     ${pathMatchRoute('/sign-in') && '!text-black !border-b-red-500'}`}
